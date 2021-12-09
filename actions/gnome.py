8b02e1a743d2f907891c7606fafdc8c06501c196
Junghyeon Park <j824h@kaist.ac.kr>
2021-12-09 12:35:49 +0900

    Handle gi failure and require pid

--- actions/gnome.py
+++ actions/gnome.py
@@ -1,13 +1,17 @@
+import logging
 import gi
 gi.require_version('Notify', '0.7')
 
 from gi.repository import Notify
 Notify.init("KREP Alerts")
 
-async def alert_terminated(pid="", name=""):
+async def alert_terminated(pid, name=""):
     if name == "":
         notification = Notify.Notification.new("Process terminated", f"PID: {pid}")
     else:
         notification = Notify.Notification.new("Process terminated", f"Process: {name}\nPID: {pid}" )
     notification.set_urgency(2)
-    notification.show()
+    try:
+        notification.show()
+    except Exception as e:
+        logging.error(e)
